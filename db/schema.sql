df1f1c73f2dd407c3c753c7ba0542710016f2869
JGuzman87 <jguzman913@gmail.com>
2025-05-02 06:19:06 -0400

    added db/schema

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE blog_posts (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
